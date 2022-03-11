32bd68880f254f898aadc195ae61603910a46731
Kirill Makhov <mmahovkirill@gmail.com>
2022-03-12 02:30:09 +0300

    feat: create Form component

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from 'react';
+import {Input} from '../index';
+import {Todo} from '../../types';
+import s from './Form.module.css';
+
+
+interface Props {
+    onSubmit: (todo: Todo) => void;
+}
+
+function Form({onSubmit}: Props) {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (event: React.FormEvent): void => {
+        event.preventDefault();
+
+        if (!title || /^\s*$/.test(title)) {
+            return;
+        }
+
+        onSubmit({
+            id: Date.now() + Math.random(),
+            title,
+            description
+        })
+
+        setTitle('');
+        setDescription('');
+    }
+
+    return (
+        <form onSubmit={handleSubmit} className={s.form}>
+            <Input
+                type="form"
+                value={title}
+                onChange={setTitle}
+                name="title"
+                placeholder="Add a todo"
+                autoFocus
+            />
+            <Input
+                type="form"
+                value={description}
+                onChange={setDescription}
+                name="description"
+                placeholder="Todo description"
+            />
+            <button type="submit" className={s.button}>
+                Add todo
+            </button>
+        </form>
+    );
+}
+
+export default Form;
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,7 @@
 export type InputType = 'form' | 'todo';
+
+export interface Todo {
+    id: number,
+    title: string,
+    description?: string,
+}
