6b8662b84cdbf1e0f5c822a0b71180207b036e3f
Kirill Makhov <mmahovkirill@gmail.com>
2022-03-12 01:51:14 +0300

    feat: create Input component

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,42 @@
+import React, {useEffect, useRef} from 'react';
+import cn from 'classnames/bind';
+import {InputType} from "../../types";
+import s from './Input.module.css';
+
+interface Props {
+    type: InputType;
+    value: string;
+    onChange: (value: string) => void;
+    onBlur?: () => void;
+    autoFocus?: boolean;
+    placeholder?: string;
+    name?: string
+}
+
+function Input({type, value, onChange, onBlur, autoFocus, placeholder, name}: Props) {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    useEffect(() => {
+        if (autoFocus) {
+            inputRef.current?.focus();
+        }
+    }, [autoFocus]);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        onChange(event.target.value);
+    }
+
+    return (
+        <input
+            placeholder={placeholder}
+            value={value}
+            onChange={handleChange}
+            name={name}
+            ref={inputRef}
+            className={cn(s.input, {[s.inputTodo]: type === 'todo'})}
+            onBlur={onBlur}
+        />
+    );
+}
+
+export default Input;
