5262cae4c37677160e9932132ad513576f4816b6
Kirill Makhov <mmahovkirill@gmail.com>
2022-03-12 15:20:20 +0300

    refactor: add export Todo component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export {default as Input} from './Input/Input';
-export {default as Form} from './Form/Form'
+export {default as Form} from './Form/Form';
+export {default as Todo} from './Todo/Todo';
