908dc0b1bb97b22da84002047ea1a3d817182b7a
Kirill Makhov <mmahovkirill@gmail.com>
2022-03-13 13:31:22 +0300

    refactor: add TodoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
+import {TodoList} from './components';
+import s from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={s.todoApp}>
+            <TodoList />
+        </div>
+    );
 }
 
 export default App;
